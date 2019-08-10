01eda39116616c4b6191ae2392a39740a40193bc
davidsth <1936038+davidsth@users.noreply.github.com>
2019-08-10 17:30:44 -0400

    Adds stylesheets to storybook html

---
+++ .storybook/config.js
@@ -0,0 +1,9 @@
+import { configure } from '@storybook/html';
+
+function loadStories() {
+  const req = require.context('../stories', true, /\.stories\.js$/);
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
+
---
+++ stories/index.stories.js
@@ -0,0 +1,11 @@
+import { storiesOf } from '@storybook/html';
+import styles from '../stylesheets/main.scss';
+
+storiesOf('Button', module)
+  .add('with text', () => '<button class="btn">Hello World</button>')
+  .add('with emoji', () => {
+    const button = document.createElement('button');
+    button.innerText = '😀 😎 👍 💯';
+    return button;
+  });
+
